82fef21570eca7f0b5e0b74320a8b4d84421a19d
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-06 13:42:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
+	if(s===""){
+		return "";
+	}
+
+	let spaceindex=s.indexOf("");
+	if(spaceindex===-1){
+		return s;
+	}
+	else{
+		s.substring(0,spaceindex);
+		
+	}
 }
 
 // Do not change the code below
